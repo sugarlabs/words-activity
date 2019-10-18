5dd2c8c8b1386e183d296061e378cab87c38adc4
James Cameron <quozl@laptop.org>
2019-10-18 19:56:33 +1100

    Remove unused PyGObject version requirement
    Was present because sugar3.speech did fail to do it, but does now.

--- wordsactivity.py
+++ wordsactivity.py
@@ -21,7 +21,6 @@ import gi
 gi.require_version('Gdk', '3.0')
 gi.require_version('Gtk', '3.0')
 gi.require_version('WebKit2', '4.0')
-gi.require_version('Gst', '1.0')
 
 from gi.repository import GObject
 GObject.threads_init()
